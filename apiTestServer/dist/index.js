bbc6892295b4ca577a92943e89062967678d8a5e
saltwalks <ewqdsacxz2345@gmail.com>
2021-07-25 23:54:31 +0900

    add JWT sign system, cookie handler

--- apiTestServer/dist/index.js
+++ apiTestServer/dist/index.js
@@ -23,47 +23,104 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var morgan_1 = __importDefault(require("morgan"));
+var cookie_parser_1 = __importDefault(require("cookie-parser"));
+var jwt = __importStar(require("jsonwebtoken"));
 var fs = __importStar(require("fs"));
+/**
+ *
+ * CONSTANTS
+ *
+ */
 var app = express_1.default();
 var port = 3000;
-var testClient = 'http://localhost:8080';
-var read = function (name) { return (fs.readFileSync(__dirname + '/json/' + name + '.json').toString()); };
-var write = function (name, data) { return (fs.writeFileSync(__dirname + '/json/' + name + '.json', data)); };
+var ACCESS_TOKEN_EXP = 60000 * 2;
+var REFRESH_TOKEN_EXP = 60000 * 5;
+var options = {
+    origin: 'http://localhost:8080',
+    credentials: true,
+    optionsSuccessStatus: 200 // 응답 상태 200으로 설정 
+};
+// TEST AES256 SECRKEY
+var SECRKEY = 'JEfWefI0E1qlnIz06qmob7cZp5IzH/i7KwOI2xqWfhE=';
+/**
+ *
+ * MIDDLEWARES
+ *
+ */
+app.use(cors_1.default(options));
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: false }));
+app.use(morgan_1.default('dev'));
+app.use(cookie_parser_1.default());
+/**
+ *
+ * LOCAL FUNCTIONS
+ *
+ */
+var read = function (name) { return (fs.readFileSync(__dirname + '/json/' + name + '.json').toString()); };
+var write = function (name, data) { return (fs.writeFileSync(__dirname + '/json/' + name + '.json', data)); };
+/**
+ *
+ * ROUTERS
+ *
+ */
 app.get('/', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', testClient);
-    res.send('aloha');
+    res.send('THOTH API TEST SERVER');
 });
-//THOTH test cases
-app.get('/all', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', testClient);
+// app.get('/note', (req, res) => {
+//     res.send(read('postit'));
+// });
+app.get(/\/note.*/, function (req, res) {
     res.send(read('postit'));
 });
-app.post('/new', function (req, res) {
+app.post('/note', function (req, res) {
     req.on("data", function (data) {
-        console.log("" + data);
+        console.log("New Note Post Requested : " + data);
         var oldJson = JSON.parse(read('postit'));
         var toPush = JSON.parse("" + data);
         oldJson.push(toPush);
         var newJson = JSON.stringify(oldJson);
         write('postit', newJson);
-        res.setHeader('Access-Control-Allow-Origin', testClient);
-        res.send("hoi");
+        res.send(JSON.stringify({ error: null }));
     });
 });
-//END THOTH test cases
-app.get('/get-test', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', testClient);
-    res.send(read('test'));
-});
-app.post('/post-test', function (req, res) {
+app.post('/signin', function (req, res) {
     req.on("data", function (data) {
-        console.log("" + data);
-        res.setHeader('Access-Control-Allow-Origin', testClient);
-        res.send("hoi");
+        console.log("Sign In Requested : " + data);
+        jwt.sign({
+            test: 'test',
+        }, SECRKEY, {
+            expiresIn: '1h',
+        }, function (err, token) {
+            if (err !== null) {
+                console.error(err);
+            }
+            else {
+                res.cookie('accessToken', token, {
+                    maxAge: ACCESS_TOKEN_EXP,
+                    path: '/',
+                });
+                res.status(200).json({
+                    error: null,
+                    token: token,
+                });
+            }
+        });
     });
 });
+app.post('/signup', function (req, res) {
+    req.on("data", function (data) {
+        console.log("Sign Up Requested : " + data);
+        res.send(JSON.stringify({ error: null }));
+    });
+});
+/**
+ *
+ * SERVER START
+ *
+ */
 app.listen(port, function () {
     console.log("[" + new Date() + "] Thoth API Test Server Started");
 });
--- apiTestServer/src/index.ts
+++ apiTestServer/src/index.ts
@@ -1,54 +1,112 @@
 import express from 'express';
+import cors from 'cors';
+import logger from 'morgan';
+import cookieParser from 'cookie-parser';
+import * as jwt from 'jsonwebtoken';
 import * as fs from 'fs';
 
+/**
+ * 
+ * CONSTANTS
+ * 
+ */
 const app = express();
 const port = 3000;
-const testClient = 'http://localhost:8080'
+const ACCESS_TOKEN_EXP = 60000*2;
+const REFRESH_TOKEN_EXP = 60000*5;
+const options = {
+    origin: 'http://localhost:8080', // 접근 권한을 부여하는 도메인
+    credentials: true, // 응답 헤더에 Access-Control-Allow-Credentials 추가
+    optionsSuccessStatus: 200 // 응답 상태 200으로 설정 
+};
+// TEST AES256 SECRKEY
+const SECRKEY = 'JEfWefI0E1qlnIz06qmob7cZp5IzH/i7KwOI2xqWfhE=';
 
+/**
+ * 
+ * MIDDLEWARES
+ * 
+ */
+app.use(cors(options));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(logger('dev'));
+app.use(cookieParser())
+
+/**
+ * 
+ * LOCAL FUNCTIONS
+ * 
+ */
 const read = (name: string) => (fs.readFileSync(__dirname + '/json/' + name + '.json').toString());
 const write = (name: string, data: string) => (fs.writeFileSync(__dirname + '/json/' + name + '.json', data));
 
-app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-
+/**
+ * 
+ * ROUTERS
+ * 
+ */
 app.get('/', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', testClient);
-    res.send('aloha');
+    res.send('THOTH API TEST SERVER');
 });
 
-//THOTH test cases
-app.get('/all', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', testClient);
+// app.get('/note', (req, res) => {
+//     res.send(read('postit'));
+// });
+
+app.get(/\/note.*/, (req, res) => {
     res.send(read('postit'));
-});
+})
 
-app.post('/new', (req, res) => {
+app.post('/note', (req, res) => {
     req.on("data", data => {
-        console.log(""+data);
+        console.log("New Note Post Requested : " + data);
         let oldJson = JSON.parse(read('postit')) as object[];
         let toPush = JSON.parse("" + data);
         oldJson.push(toPush);
         let newJson = JSON.stringify(oldJson);
         write('postit', newJson);
-        res.setHeader('Access-Control-Allow-Origin', testClient);
-        res.send("hoi");
+        res.send(JSON.stringify({error: null}));
     })
 });
-//END THOTH test cases
 
-app.get('/get-test', (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', testClient);
-    res.send(read('test'));
-});
-
-app.post('/post-test', (req, res) => {
+app.post('/signin', (req, res) => {
     req.on("data", data => {
-        console.log(""+data);
-        res.setHeader('Access-Control-Allow-Origin', testClient);
-        res.send("hoi");
+        console.log("Sign In Requested : " + data);
+        jwt.sign({
+            test: 'test',
+        }, SECRKEY, {
+            expiresIn: '1h',
+        }, (err, token) => {
+            if(err !== null) {
+                console.error(err);
+            } else {
+                res.cookie('accessToken', token, {
+                    maxAge: ACCESS_TOKEN_EXP,
+                    path: '/',
+                });
+                res.status(200).json({
+                        error: null,
+                        token,
+                    }
+                );
+            }
+        });
     })
 });
 
+app.post('/signup', (req, res) => {
+    req.on("data", data => {
+        console.log("Sign Up Requested : " + data);
+        res.send(JSON.stringify({ error: null }));
+    })
+});
+
+/**
+ * 
+ * SERVER START
+ * 
+ */
 app.listen(port, () => {
     console.log(`[${new Date()}] Thoth API Test Server Started`);
 });
