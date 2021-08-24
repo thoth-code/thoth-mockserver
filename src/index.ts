9e10564f45d282f4cb3146af95774260208840c6
haeramkeem <ewqdsacxz2345@gmail.com>
2021-08-24 14:00:56 +0000

    fix: simplify mock & handling content-type application/json

--- src/index.ts
+++ src/index.ts
@@ -39,7 +39,6 @@ app.use(cookieParser())
  * 
  */
 const read = (name: string) => (fs.readFileSync(__dirname + '/json/' + name + '.json').toString());
-const write = (name: string, data: string) => (fs.writeFileSync(__dirname + '/json/' + name + '.json', data));
 
 /**
  * 
@@ -51,52 +50,41 @@ app.get('/', (req, res) => {
 });
 
 app.get(/\/api\/notes.*/, (req, res) => {
-    console.log("Search Note Requested:", req.url);
+    console.log("Search Note Requested: " + req.url);
     res.send(read('postit'));
 });
 
 app.post('/api/note', (req, res) => {
-    req.on("data", data => {
-        console.log("New Note Post Requested : " + data);
-        let oldJson = JSON.parse(read('postit')) as object[];
-        let toPush = JSON.parse("" + data);
-        oldJson.push(toPush);
-        let newJson = JSON.stringify(oldJson);
-        write('postit', newJson);
-        res.send(JSON.stringify({error: null}));
-    });
+    console.log("New Note Post Requested : " + req.body);
+    res.send(JSON.stringify({error: null}));
 });
 
 app.post('/api/signin', (req, res) => {
-    req.on("data", data => {
-        console.log("Sign In Requested : " + data);
-        jwt.sign({
-            uid: 'test-uid-200',
-        }, SECRKEY, {
-            expiresIn: ACCESS_TOKEN_EXP,
-        }, (err, token) => {
-            if(err !== null) {
-                console.error(err);
-            } else {
-                res.cookie('accessToken', token, {
-                    maxAge: ACCESS_TOKEN_EXP,
-                    path: '/',
-                });
-                res.status(200).json({
-                        error: null,
-                        token,
-                    }
-                );
-            }
-        });
+    console.log("Sign in requested : " + req.body);
+    jwt.sign({
+        uid: 'test-uid-200',
+    }, SECRKEY, {
+        expiresIn: ACCESS_TOKEN_EXP,
+    }, (err, token) => {
+        if(err !== null) {
+            console.error(err);
+        } else {
+            res.cookie('accessToken', token, {
+                maxAge: ACCESS_TOKEN_EXP,
+                path: '/',
+            });
+            res.status(200).json({
+                    error: null,
+                    token,
+                }
+            );
+        }
     });
 });
 
 app.post('/api/signup', (req, res) => {
-    req.on("data", data => {
-        console.log("Sign Up Requested : " + data);
-        res.send(JSON.stringify({ error: null }));
-    });
+    console.log("Sign Up Requested : " + req.body);
+    res.send(JSON.stringify({ error: null }));
 });
 
 app.delete(/\/api\/note\/.*/, (req, res) => {
@@ -105,21 +93,15 @@ app.delete(/\/api\/note\/.*/, (req, res) => {
 });
 
 app.put('/api/note', (req, res) => {
-    req.on("data", data => {
-        console.log("Edit note requested : " + data);
-        res.send(JSON.stringify({ error: null }));
-    });
+    console.log("Edit note requested : " + req.body);
+    res.send(JSON.stringify({ error: null }));
 });
 
-// TODO: attach note to my board
 app.post('/api/myboard', (req, res) => {
-    req.on("data", data => {
-        console.log("Attach note requested : " + data);
-        res.send(JSON.stringify({ error: null }));
-    });
+    console.log("Attach note requested : " + req.body);
+    res.send(JSON.stringify({ error: null }));
 });
 
-// TODO: show my board
 app.get('/api/myboard', (req, res) => {
     console.log("MyBoard requested : " + JSON.stringify(req.cookies));
     res.send(read('postit'));
